3131ef9b73093cb97f160265a251664c5a19e6c5
Hamza <khanzadahamza76@gmail.com>
2024-11-18 11:04:58 +0500

    my first php program

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "hello-world";
+echo "<br/>";
+echo "Name is Hamza";
+echo "<br/>";
+echo "City is Karachi";
+echo "<br/>";
+echo "Job is Front-end-developer";
+echo "<br/>";
+echo("function is");
+
+?>
