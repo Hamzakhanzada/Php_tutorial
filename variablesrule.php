3076fd19ce358354832a48c28ce17007c22835d6
Hamza <khanzadahamza76@gmail.com>
2024-11-18 12:17:12 +0500

    variable-rule

---
+++ variablesrule.php
@@ -0,0 +1,8 @@
+<!-- A variables start with the $ Sign -->
+<!-- A variable name cannot start with a number -->
+<!-- A variable name must start with a letter or a underscore (_) character -->
+<!-- A variable name cannot start with a special character -->
+<!-- A variable name can only contain alpha numeric characters and underscores(_) -->
+<!-- $name $name $NAME are the three differents variables -->
+<!-- variables Syntax -->
+<!-- $name = "anil"; -->
