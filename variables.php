99d0e8f1692b42190fa6f0ec4cf62ba6863ca92b
Hamza <khanzadahamza76@gmail.com>
2024-11-18 11:27:20 +0500

    declare variables

---
+++ variables.php
@@ -0,0 +1,22 @@
+<?php
+$name = "khan";
+echo $name;
+echo "<br/>";
+echo $name;
+echo "<br/>";
+echo $name;
+echo "<br/>";
+echo $name;
+echo "<br/>";
+echo $name;
+?>
+<?php
+$num = 5;
+echo "<br/>";
+echo $num * 30;
+echo "<br/>";
+echo $num * 40;
+echo "<br/>";
+echo $num * 50;
+echo "<br/>";
+?>
